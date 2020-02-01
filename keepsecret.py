af031022e69ad55b97b97d474de55ae06433a1fc
Vinicius Torino <vini.torino.lab@gmail.com>
2020-02-01 12:45:54 -0300

    import cryptography libraries

--- keepsecret.py
+++ keepsecret.py
@@ -1,15 +1,42 @@
 import sys, getpass, os , stat, crypt
 from hmac import compare_digest as check_hash
 from time import sleep 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
+import base64
+
 
 #def build_keeper():
 
+"""
+shadow = 'shadow'
+def grep(secrets, key):
+    for key,value in secrets:
+        if key in secrets:
+            print(value)
 
-# shadow = 'shadow'
-#def grep(secrets, key):
-#    for key,value in secrets:
-#        if key in secrets:
-#            print(value)
+
+def get_secret(shadow, secrets, key):
+    secret_value = None 
+    if check_pw_verbose():
+        secret_value = grep(secrets, key)
+        if value not None:
+            return value
+    else:
+        sys.exit()
+
+def set_secret(shadow, secrets, key, value):
+    if check_pw_verbose():
+        if secrets.get(key, None) is None:
+            secrets[key] = value
+        els            sys.exit()
+        # file system is alredy mounted 
+        # is under a folder on the some dir
+        print(secrets)
+"""
+
 
 def check_perm(shadow):
     mode = os.stat(shadow)
@@ -36,25 +63,6 @@ def check_pw_verbose(shadow):
             return False
     else:
         return False
-
-def get_secret(shadow, secrets, key):
-    secret_value = None 
-    if check_pw_verbose():
-        secret_value = grep(secrets, key)
-        if value not None:
-            return value
-    else:
-        sys.exit()
-
-def set_secret(shadow, secrets, key, value):
-    if check_pw_verbose():
-        if secrets.get(key, None) is None:
-            secrets[key] = value
-        else:
-            sys.exit()
-        # file system is alredy mounted 
-        # is under a folder on the some dir
-        print(secrets)
 
 
 def gen_hash(pw1):
@@ -89,21 +97,22 @@ def insert_pw(shadow, pw1):
         return False 
 
 def set_pw(shadow):
-    for count in range(3):
-        print('Changing password for Keepsecret: ')
-        pw0 = getpass.getpass('(current) Keepsecret password: ')
-        if check_pw(shadow ,pw0):
-            pw1 = getpass.getpass('Enter new Keepsecret password: ')
-            pw2 = getpass.getpass('Retype new Keepsecret password: ')
-            if pw1 == pw2:
-                if insert_pw(shadow, pw1):
-                    print('password updated successfully')
-                    break
+        for count in range(3):
+            print('Changing password for Keepsecret: ')
+            pw0 = getpass.getpass('(current) Keepsecret password: ')
+            if check_pw(shadow ,pw0):
+                pw1 = getpass.getpass('Enter new Keepsecret password: ')
+                pw2 = getpass.getpass('Retype new Keepsecret password: ')
+                if pw1 == pw2:
+                    if insert_pw(shadow, pw1):
+                        print('password updated successfully')
+                        break
+                    else:
+                        print('Insert Failed!', file=sys.stderr)
+                        break
                 else:
-                    print('Insert Failed!', file=sys.stderr)
-                    break
-            else:
-                print('Password unchanged', file=sys.stderr)
+                    print('Password unchanged', file=sys.stderr)
         else:
             print('Sorry, try again', file=sys.stderr)
-        sleep(2)        
+        sleep(2)
+
