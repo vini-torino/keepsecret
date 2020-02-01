3793df4f952963369ded86f2cb72a62ca96fe500
Vinicius Torino <vini.torino.lab@gmail.com>
2020-02-01 18:19:16 -0300

    We remove a lot of lines

--- keepsecret.py
+++ keepsecret.py
@@ -1,11 +1,23 @@
+#!/usr/bin/python3
+# - * - encode: utf-8  - * -
 import sys, getpass, os , stat, crypt
 from hmac import compare_digest as check_hash
-from time import sleep 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 import base64
+import getopt
+
+def check_perm(shadow):
+    mode = os.stat(shadow)
+    chmod = oct(mode.st_mode)[4:]
+    if chmod != '0600':
+        print('Permissions '+ chmod +' for' + shadow + ' are too open.')
+        print('It is required that the Keepsecret shadow file are NOT accessible by others')
+        print('This file will be ignored')
+        return False
+    return True
 
 def cat_bytes(secret_file):
     with open(secret_file, 'rb') as f:
@@ -31,105 +43,28 @@ def get_key(pw0):
     key = base64.urlsafe_b64encode(kdf.derive(pw0_bytes))
     return key
 
-def open_vault(pw0, secret_file, is_plain):
+def manage_vault(pw0, secret_file, is_open):
     key = get_key(pw0)
     data = cat_bytes(secret_file)
     fernet_object = Fernet(key)
-    if is_plain:
+    if is_open:
         encrypted = fernet_object.encrypt(data)
         write_bytes(secret_file, encrypted)
     else:
         decrypted = fernet_object.decrypt(data)
         write_bytes(secret_file, decrypted)
 
-def encrypt_vault(pw0, secret_file):
-    open_vault(pw0, secret_file, True)
-
-def decrypt_vault(pw0, secret_file):
-    open_vault(pw0, secret_file, False)
-
-
-def check_perm(shadow):
-    mode = os.stat(shadow)
-    chmod = oct(mode.st_mode)[4:]
-    if chmod != '0600':
-        print('Permissions '+ chmod +' for' + shadow + ' are too open.')
-        print('It is required that the Keepsecret shadow file are NOT accessible by others')
-        print('This file will be ignored')
-        return False
-    else:
-        return True
-
-def open_secrets(shadow, secret_file):
-    # We should  check mod of Keepsecret shadow file first
-    # it must be 0600
+def manage_secrets(shadow, secret_file, is_open):
     if check_perm(shadow):
         with open(shadow, 'r') as f:
             data = f.read()
             f.close()
         old_hash = data.rstrip()
-        pw0 = getpass.getpass('Insert your password to access the  vault: ')
+        pw0 = getpass.getpass('Insert your password to access your secrets: ')
         if check_hash(old_hash, crypt.crypt(pw0, old_hash)):
-            decrypt_vault(pw0, secret_file)
-            return True
-        else:
-            return False
-    else:
-        return False
-
-def close_secrets(shadow, secret_file):
-    # We should  check mod of Keepsecret shadow file first
-    # it must be 0600
-    if check_perm(shadow):
-        with open(shadow, 'r') as f:
-            data = f.read()
-            f.close()
-        old_hash = data.rstrip()
-        pw0 = getpass.getpass('Insert your password to hide your secrets: ')
-        if check_hash(old_hash, crypt.crypt(pw0, old_hash)):
-            encrypt_vault(pw0, secret_file)
-            return True
-        else:
-            return False
-    else:
-        return False
-
-def gen_hash(pw1):
-    passwd = crypt.crypt(pw1, crypt.mksalt(crypt.METHOD_SHA512))
-    return passwd
-
-def check_pw(shadow, pw0, secret_file):
-    # We should  check mod of Keepsecret shadow file first
-    # it must be 0600
-    if check_perm(shadow):
-        with open(shadow, 'r') as f:
-            data = f.read()
-            f.close()
-        old_hash = data.rstrip()
-        if check_hash(old_hash, crypt.crypt(pw0, old_hash)):
-            decrypt_vault(pw0, secret_file)
-            return True
-        else:
-            return False
-    else:
-        return False
-
-def insert_pw(shadow, pw1, secret_file):
-    # Your should check mod of Keepsecret shadow file first
-    # It must be 0600
-    if check_perm(shadow):
-        hashed = gen_hash(pw1)
-        encrypt_vault(pw1, secret_file)
-        with open(shadow, 'w') as f:
-            f.write( hashed + '\n')
-            f.close()
-        return True
-    else:
-        return False 
+            manage_vault(pw0, secret_file, is_open)
 
 def new_pw(shadow, secret_file):
-    # Your should check mod of Keepsecret shadow file first
-    # It must be 0600
     if check_perm(shadow):
         pw1 = getpass.getpass('Insert your password to generate keepsecret vault: ')
         hashed = crypt.crypt(pw1, crypt.mksalt(crypt.METHOD_SHA512))
@@ -138,24 +73,24 @@ def new_pw(shadow, secret_file):
             f.write( hashed + '\n')
             f.close()
 
+shadow = 'shadow'
+argv = sys.argv[1:]
+try:
+    opts, args = getopt.getopt(argv, "n:e:d:", ["new=", "encrypt=", "decrypt"])
+except getopt.GetoptError as err:
+    print(err)
+    opts = []
 
-def set_pw(shadow, secret_file):
-        for i in range(3):
-            print('Changing password for Keepsecret: ')
-            pw0 = getpass.getpass('(current) Keepsecret password: ')
-            if check_pw(shadow ,pw0, secret_file):
-                pw1 = getpass.getpass('Enter new Keepsecret password: ')
-                pw2 = getpass.getpass('Retype new Keepsecret password: ')
-                if pw1 == pw2:
-                    if insert_pw(shadow, pw1, secret_file):
-                        print('password updated successfully')
-                        break
-                    else:
-                        print('Insert Failed!', file=sys.stderr)
-                        break
-                else:
-                    print('Password unchanged', file=sys.stderr)
-        else:
-            print('Sorry, try again', file=sys.stderr)
-        sleep(2)
-
+for opt, arg in opts:
+    if opt in ['-n', '--new']:
+        #do something
+        new_pw(shadow, arg)
+        print(f'setting new password for {arg}')
+    elif opt in ['-e', '--encrypt' ]:
+        #do something
+        manage_secrets(shadow, arg, True)
+        print(f'encrypting {arg}')
+    elif opt in ['-d', '--decrypt']:
+        #do something
+        manage_secrets(shadow, arg, False)
+        print(f'decrypting {arg}')
